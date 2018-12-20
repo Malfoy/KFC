2b8a5bdcab167951a800ecc329ecf7e9f2eaee39
Malfoy <antoine.limasset@gmail.com>
2018-12-20 14:20:48 +0100

    version with one MPHF is here

--- index_min.cpp
+++ index_min.cpp
@@ -10,9 +10,22 @@
 #include <chrono>
 #include <map>
 #include <set>
+#include "index_min.h"
 
 
 using namespace std;
 using namespace chrono;
 
 
+void index_full::insert(uint64_t kmer){
+	uint64_t pos(Hash.lookup(kmer));
+	if(Values[pos].kmer==kmer){
+		++Values[pos].count;
+	}
+}
+
+void index_full::dump_counting(){
+	for(uint i(0);i<Values.size();++i){
+		cout<<Values[i].kmer<<" "<<Values[i].count<<"\n";
+	}
+}
