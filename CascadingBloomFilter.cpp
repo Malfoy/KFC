16870b5f99abeefc4d658e9544bc78c80afa431a
Mikael Salson <mikael.salson@univ-lille1.fr>
2018-12-20 20:29:27 +0100

    CascadingBF: BF were all sharing the same BitSet (copying pointer)

--- CascadingBloomFilter.cpp
+++ CascadingBloomFilter.cpp
@@ -11,7 +11,9 @@ using namespace std;
 CascadingBloomFilter::CascadingBloomFilter(uint64_t size, uint8_t num_blooms,
                                            float reset_ratio) {
   // Bloom filters init
-  this->filters = vector<BloomFilter>(num_blooms, BloomFilter(size/num_blooms, NUM_HASH, reset_ratio));
+  this->filters = vector<BloomFilter>(num_blooms);
+  for (uint i = 0; i < num_blooms; i++)
+    this->filters[i] = BloomFilter(size/num_blooms, NUM_HASH, reset_ratio);
   this->saved = vector<bool>(size/num_blooms, false);
   this->kmers = vector<uint64_t>();
 
