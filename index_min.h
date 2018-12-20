3a3e9d43b591b8fbf3633602a4d556d2393a144a
Malfoy <antoine.limasset@gmail.com>
2018-12-20 14:20:48 +0100

    version with one MPHF is here

--- index_min.h
+++ index_min.h
@@ -27,7 +27,12 @@ class index_full {
   public:
 	MPHF Hash;
 	vector<value> Values;
-	index_full(const vector<uint64_t>& V) { Values.resize(V.size()); }
+
+	index_full(const vector<uint64_t>& V) {
+		Values.resize(V.size());
+		auto data_iterator = boomphf::range(static_cast<const uint64_t*>(&((V)[0])), static_cast<const uint64_t*>((&(V)[0]) + V.size()));
+		Hash = boomphf::mphf<uint64_t, hasher>(V.size(), data_iterator, 4, 5, false);
+	}
 	void insert(uint64_t);
 	void dump_counting();
 };
