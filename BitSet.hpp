58fbd8878f9f7445e9cd9902b17bd9c909a6c613
Mikael Salson <mikael.salson@univ-lille1.fr>
2018-12-20 18:33:55 +0100

    BitSet classes

---
+++ BitSet.hpp
@@ -0,0 +1,25 @@
+#include <cstdint>
+
+#define WORD_SIZE (sizeof(uint64_t) * 8)
+/* reads bit p from e */
+#define bitget(e, p) ((((e)[(p) / WORD_SIZE] >> ((p) % WORD_SIZE))) & 1)
+/* sets bit p in e */
+#define bitset(e, p) ((e)[(p) / WORD_SIZE] |= (1 << ((p) % WORD_SIZE)))
+
+class BitSet {
+  public:
+	BitSet(uint64_t nb_bits);
+	~BitSet();
+
+	bool get(uint64_t pos) const;
+	void set(uint64_t pos);
+
+	void reset();
+
+	uint64_t size() const;
+
+  private:
+	uint64_t* m_bits;
+	uint64_t m_nb_bits;
+	uint64_t m_int_size;
+};
