750abea9e90843c180a49680628ac1c3fc737b0f
camillemarchet <marchetcamille@gmail.com>
2019-02-15 15:44:19 +0100

    removed dangerous parameters

--- evaluator.cpp
+++ evaluator.cpp
@@ -78,19 +78,21 @@ vector<string> split(const string& s, char delim) {
 }
 
 int main(int argc, char** argv) {
-	if (argc < 4) {
-		cout << "[kmer count file to evaluate] [reference kmer count file] [k value] [core number] [n for 2^n pass]" << endl;
+	if (argc < 3) {
+		//~ cout<<"[kmer count file to evaluate] [reference kmer count file] [k value] [core number] [n for 2^n pass]"<<endl;
+		cout << "[kmer count file to evaluate] [reference kmer count file] [k value] " << endl;
 		exit(0);
 	}
 	auto start = chrono::system_clock::now();
 	string inputUnitig(argv[1]);
 	string inputRef(argv[2]);
 	unsigned k(stoi(argv[3]));
-	unsigned n(0);
-	unsigned nb_cores(stoi(argv[4]));
-	if (argc > 5) {
-		n = (stoi(argv[5]));
-	}
+	unsigned n(1);
+	unsigned nb_cores(1);
+	//~ unsigned nb_cores(stoi(argv[4]));
+	//~ if(argc>5){
+	//~ n=(stoi(argv[5]));
+	//~ }
 	unsigned nbHash = 1 << n;
 	cout << "I will perform " << nbHash << " pass" << endl;
 	srand(time(NULL));
