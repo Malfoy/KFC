df99fcd5609dd3735961f4f08ce0c059e223bf4b
Maël Kerbiriou <mael.kerbiriou@free.fr>
2019-03-06 11:27:01 +0000

    Add Quotiented Spectral Bloom Filter

---
+++ blocked.hpp
@@ -0,0 +1,277 @@
+#ifndef BLOCKEDBLOOM_HPP
+#define BLOCKEDBLOOM_HPP
+
+#include <cstdint>
+#include <climits>
+#include <cstdlib>
+
+#include <memory>
+#include <exception>
+#include <iostream>
+
+#include "MurmurHash3.h"
+#undef assert // FIXME upstream
+#include "gatbl/common.hpp"
+
+/**
+ * Type traits for mutating integers of small bitwidth
+ * The bitwidth can be as small as 1bit and as large as the bitwidth of the word representation, but must be constant.
+ * This is the generic implementation with bit masking.
+ */
+template<size_t _bit_width, typename _word_t = uint64_t, typename = void>
+struct subword_traits {
+	using word_t = _word_t;
+	static constexpr size_t subword_bits = _bit_width;
+	static constexpr size_t word_bits = CHAR_BIT * sizeof(word_t);
+	static constexpr size_t nsubwords = word_bits / subword_bits;
+	static_assert(subword_bits < word_bits, "Subword should be smaller than the word");
+	static_assert(word_bits % subword_bits == 0, "Subword bitwidth must divide the word width");
+
+	/// A reference to a constant bit slice
+	struct const_reference {
+		const_reference(const word_t& word, size_t subword_idx)
+		  : m_word(word)
+		  , m_offset(static_cast<uint8_t>(subword_bits * subword_idx)) {
+			assume(subword_idx < nsubwords, "Access out of bound of word");
+		}
+
+	  public:
+		operator word_t() const { return (m_word >> m_offset) & m_mask; }
+
+	  protected:
+		const word_t& m_word;
+		const uint8_t m_offset;
+	};
+
+	/// A reference to a bit slice
+	struct reference {
+		reference(word_t& word, size_t subword_idx)
+		  : m_word(word)
+		  , m_offset(static_cast<uint8_t>(subword_bits * subword_idx)) {
+			assume(subword_idx < nsubwords, "Access out of bound of word");
+		}
+
+		reference& operator=(word_t val) {
+			assume(val <= m_mask, "Value %llu is not representable in %u bits", val, this->m_bits);
+			m_word &= ~(m_mask << m_offset);
+			m_word |= val << m_offset;
+			return *this;
+		}
+
+		operator word_t() const { return (m_word >> m_offset) & m_mask; }
+
+	  protected:
+		word_t& m_word;
+		const uint8_t m_offset;
+	};
+
+	static reference at(word_t& word, size_t idx) { return {word, idx}; }
+	static const_reference at(const word_t& word, size_t idx) { return {word, idx}; }
+
+  private:
+	static constexpr word_t m_mask = (word_t(1) << subword_bits) - 1;
+};
+
+/// The trivial case where subword == word
+template<size_t _bit_width, typename _word_t>
+struct subword_traits<_bit_width, _word_t, std::enable_if<_bit_width == sizeof(_word_t) * CHAR_BIT>> {
+	using word_t = _word_t;
+	using reference = word_t&;
+	using const_reference = const word_t&;
+
+	static reference at(word_t& word, size_t idx) { return word; }
+	static const_reference at(const word_t& word, size_t idx) { return word; }
+
+	static constexpr size_t word_bits = CHAR_BIT * sizeof(word_t);
+	static constexpr size_t subword_bits = word_bits;
+	static constexpr size_t nsubwords = 1;
+};
+
+// TODO: 8 bit version
+
+#ifndef __cpp_lib_hardware_interference_size
+static constexpr size_t hardware_constructive_interference_size = 64;
+#endif
+
+/// A cacheline of integers mutated through a subword_traits
+template<typename _subword_traits, size_t block_bytes = hardware_constructive_interference_size>
+struct Block : public _subword_traits {
+	using typename _subword_traits::const_reference;
+	using typename _subword_traits::reference;
+	using typename _subword_traits::word_t;
+	static constexpr size_t nwords = block_bytes / sizeof(word_t);
+	static constexpr size_t nsubwords = _subword_traits::nsubwords * nwords;
+
+	const_reference at(size_t idx) const {
+		assume(idx <= nsubwords, "Access out of bound in block %llu > %llu", idx, nwords);
+		return const_reference(m_arr[idx / _subword_traits::nsubwords], idx % _subword_traits::nsubwords);
+	}
+	reference at(size_t idx) {
+		assume(idx <= nsubwords, "Access out of bound in block %llu > %llu", idx, nwords);
+		return reference(m_arr[idx / _subword_traits::nsubwords], idx % _subword_traits::nsubwords);
+	}
+
+  private:
+	word_t m_arr[nwords] = {0};
+};
+
+// Before C++17 new doesn't correctly align over-aligned type, so we use posix_memalign and a free() based deallocator
+template<typename T>
+struct FreeDelete {
+	void operator()(T* ptr) const {
+		ptr->~T();
+		free(ptr);
+	}
+};
+
+template<typename T>
+using aligned_array = std::unique_ptr<T[], FreeDelete<T>>;
+
+template<typename T>
+aligned_array<T> make_unique_aligned(size_t n) {
+	void* void_ptr;
+	if (posix_memalign(&void_ptr, alignof(T), n * sizeof(T)) != 0) {
+		throw std::bad_alloc();
+	}
+	auto ptr = aligned_array<T>(static_cast<T*>(void_ptr));
+	// Initialize the items with placement new and default constructor
+	for (unsigned i = 0; i < n; i++) {
+		new (&ptr[i]) T{};
+	}
+
+	return ptr;
+}
+
+/**
+ * @brief A vector of overaligned items
+ */
+template<typename T>
+class AlignedVector {
+  public:
+	using element_type = T;
+
+	AlignedVector(uint64_t n)
+	  : m_size(n) {
+		m_arr = make_unique_aligned<element_type>(n);
+	}
+
+	size_t size() const { return m_size; }
+	size_t nbytes() const { return sizeof(element_type) * m_size; }
+
+	const element_type& at(size_t i) const {
+		assume(i < m_size, "indice out of range: %llu >= %llu", i, m_size);
+		return m_arr[i];
+	}
+	element_type& at(size_t i) {
+		assume(i < m_size, "indice out of range: %llu >= %llu", i, m_size);
+		return m_arr[i];
+	}
+
+  private:
+	aligned_array<element_type> m_arr;
+	const size_t m_size;
+};
+
+template<uint8_t counter_bits = 2, uint8_t hash_funcs = 3>
+class BlockedCMS {
+	using subword_trait = subword_traits<counter_bits>;
+	using block_t = Block<subword_trait>;
+	using array_t = AlignedVector<block_t>;
+	using word_t = typename subword_trait::word_t;
+
+	static constexpr size_t ncounter_per_block = block_t::nsubwords;
+
+  public:
+	static constexpr word_t max_count = (word_t(1) << counter_bits) - 1;
+	static constexpr size_t block_bytes = sizeof(block_t);
+
+	/// @param log2size: log2 of the number of block
+	BlockedCMS(uint8_t log2size)
+	  : m_arr(size_t(1) << log2size)
+	  , m_log2size(log2size) {}
+
+	word_t aproximate(void* data, size_t len) {
+		__uint128_t hash0;
+		MurmurHash3_x64_128(data, static_cast<int>(len), 0, &hash0);
+
+		block_t& block = m_arr.at(static_cast<size_t>(hash0) % (size_t(1) << m_log2size));
+		hash0 >>= m_log2size;
+
+		__uint128_t hash = hash0;
+		auto min_val = ~word_t(0);
+		for (unsigned i = 0; i < hash_funcs; i++) {
+			size_t counter_idx = hash % ncounter_per_block;
+			hash /= ncounter_per_block;
+
+			word_t val = block.at(counter_idx * counter_bits, counter_bits);
+			min_val = val < min_val ? val : min_val;
+		}
+
+		return min_val;
+	}
+
+	/**
+	 * Increment the counter
+	 * @returns the previous value
+	 */
+	word_t add(void* data, size_t len) {
+		__uint128_t hash0;
+		MurmurHash3_x64_128(data, static_cast<int>(len), 0, &hash0);
+
+		block_t& block = m_arr.at(static_cast<size_t>(hash0) % (size_t(1) << m_log2size));
+		hash0 >>= m_log2size;
+
+		__uint128_t hash = hash0;
+		auto min_val = ~word_t(0);
+		for (unsigned i = 0; i < hash_funcs; i++) {
+			word_t val = block.at(hash % ncounter_per_block);
+			hash /= ncounter_per_block;
+
+			min_val = val < min_val ? val : min_val;
+		}
+
+		if (min_val == max_count) return min_val;
+
+		hash = hash0;
+		for (unsigned i = 0; i < hash_funcs; i++) {
+			auto ref = block.at(hash % ncounter_per_block);
+			hash /= ncounter_per_block;
+
+			word_t val = ref;
+			if (val == min_val) ref = val + 1;
+			// else if(val > min_val) ref = val - 1;
+		}
+
+		return min_val;
+	}
+
+	std::array<size_t, max_count + 1> histogram() {
+		std::array<size_t, max_count + 1> res{};
+		for (size_t i = 0; i < m_arr.size(); i++) {
+			auto block = m_arr.at(i);
+			for (size_t j = 0; j < ncounter_per_block; j++) {
+				word_t val = block.at(j);
+				res[val]++;
+			}
+		}
+		return res;
+	}
+
+	size_t ncounters() const { return m_arr.size() * ncounter_per_block; }
+
+	friend std::ostream& operator<<(std::ostream& out, BlockedCMS& cms) {
+		std::array<size_t, max_count + 1> hist = cms.histogram();
+		for (size_t i = 0; i <= max_count; i++)
+			out << "\tcount" << i << ":\t" << hist[i] << std::endl; //(hist[i] * 100) / ncounters << std::endl;
+		return out;
+	}
+
+  private:
+	array_t m_arr;
+	const uint8_t m_log2size;
+};
+
+class BlockedBloom { /*TODO*/
+};
+
+#endif // BLOCKEDBLOOM_HPP
